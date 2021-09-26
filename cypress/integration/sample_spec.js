0841d401b4d5680d616ff14a3ac4bba2769aaaa9
ferdyfebriyanto <ferdyfebriyanto22@gmail.com>
2021-09-26 17:30:46 +0700

    Query untuk sebuah elemen

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -2,6 +2,6 @@ describe('My First Test', () => {
     it('Finds the content "type"', () => {
         cy.visit('https://example.cypress.io')
 
-        cy.contains('type').click()
+        cy.contains('type')
     })
 })
