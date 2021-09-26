1fcdce0d2bbb3696fe79b451df04cb5573599b18
ferdyfebriyanto <ferdyfebriyanto22@gmail.com>
2021-09-26 18:25:44 +0700

    memberikan komentar

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,17 +1,15 @@
 describe('My First Test', () => {
     it('Gets, type and asserts', () => {
-        cy.visit('https://example.cypress.io')
+        cy.visit('https://example.cypress.io') // untuk mengunjungi url
 
-        cy.pause()
-
-        cy.contains('type').click()
+        cy.contains('type').click() // contains() untuk mencari elemen, sedangkan click() untuk perintah tindakan
 
         // Should be on a new URL which includes '/commands/actions'
-        cy.url().should('include', '/commands/actions')
+        cy.url().should('include', '/commands/actions') // untuk memverifikasi url baru
 
         // Get an input, type into it and verify that the value has been updated
-        cy.get('.action-email')
-            .type('fake@email.com')
-            .should('have.value', 'fake@email.com')
+        cy.get('.action-email') //cy.get() : untuk memilih elemen berdasarkan kelas CSS
+            .type('fake@email.com') //.type() : untuk memasukkan teks ke dalam inputan yang dipilih
+            .should('have.value', 'fake@email.com') //.should : untuk memverifikasi bahwa nilai input mencerminkan teks yang diketik
     })
 })
