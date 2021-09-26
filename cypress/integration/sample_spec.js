e5611c1cb49544d9b16826d31a8c8e75c267e642
ferdyfebriyanto <ferdyfebriyanto22@gmail.com>
2021-09-26 17:31:26 +0700

    Klik sebuah elemen

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -2,6 +2,6 @@ describe('My First Test', () => {
     it('Finds the content "type"', () => {
         cy.visit('https://example.cypress.io')
 
-        cy.contains('type')
+        cy.contains('type').click()
     })
 })
